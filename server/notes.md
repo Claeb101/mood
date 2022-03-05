814c98556eda6c04fcdea46e524ea377525682a9
Claeb101 <calebarulandu@gmail.com>
2022-03-05 18:02:19 -0500

    basic functionality

---
+++ server/notes.md
@@ -0,0 +1,8 @@
+server:
+
+client:
+join room:
+    - ask server if room exists if it does, use returned
+
+do i need a db?
+    - the room only needs to stay for like 10 mins max
---
+++ server/src/index.js
@@ -0,0 +1,60 @@
+import express from "express"
+import cors from 'cors'
+import {v4 as uuid} from 'uuid'
+import { Server } from 'socket.io'
+import http from 'http'
+
+const app = express()
+const server = http.createServer(app)
+
+app.use(cors())
+app.use(express.json())
+
+const rooms = {}
+
+app.post('/room', (req, res) => {
+  // use or create new join code
+  let joinCode = req.body.joinCode
+  if(!joinCode){
+    joinCode = ''
+    for(let i = 0; i < 5; i++) joinCode += String.fromCharCode('A'.charCodeAt(0)+Math.floor(Math.random()*26))
+  }
+
+  // create room
+  if(!rooms[joinCode]){
+    const io = new Server(server, {
+      cors: {
+        origin: '*',
+      },
+      transports: ['websocket'],
+      path: `/${joinCode}/`
+    })
+    
+    io.on('connection', (socket) => {
+      console.log(`a user connected to room ${joinCode}`)
+    
+      socket.on('message', message => {
+        console.log(message)
+        const vote = JSON.parse(message)
+        vote.id = uuid()
+        rooms[joinCode].data.votes.push(vote)
+        io.emit('message', JSON.stringify(vote)) // forward vote to clients
+      })
+    })
+  
+    rooms[joinCode] = {
+      server: io,
+      joinCode: joinCode,
+      data: {
+        votes: []
+      }
+    }
+  }
+  
+  res.status(200).json({
+    joinCode: joinCode,
+    data: {...rooms[joinCode].data}
+  })
+})
+
+server.listen(8080, () => console.log('listening on http://localhost:8080'))
